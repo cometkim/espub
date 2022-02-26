4b284a0e8ea8e55985e96acceeef6d10b8d47658
Hyeseong Kim <hey@hyeseong.kim>
2022-02-26 17:13:41 +0900

    fix build target resolutions
    - Support `ios_safari`, `android`, `and_chr` and `and_ff` query
    - Drop `deno` query
    
    Fixed #33

---
+++ .changeset/tender-dryers-retire.md
@@ -0,0 +1,8 @@
+---
+"nanobundle": patch
+---
+
+fix build target resolutions
+
+- Support `ios_safari`, `android`, `and_chr` and `and_ff` query
+- Drop `deno` query
--- src/target.ts
+++ src/target.ts
@@ -1,10 +1,26 @@
 import browserslist from 'browserslist';
 
-const supportedPlatforms = ['chrome', 'firefox', 'safari', 'edge', 'node', 'deno'] as const;
-type SupportedPlatform = typeof supportedPlatforms[number];
+type SupportedBrowser = keyof typeof browsersToTargets;
+type EsbuildTarget = typeof browsersToTargets[SupportedBrowser];
 
-function isSupported(platform: string): platform is SupportedPlatform {
-  return supportedPlatforms.includes(platform as SupportedPlatform);
+const browsersToTargets = {
+  'chrome': 'chrome',
+  'firefox': 'firefox',
+  'safari': 'safari',
+  'edge': 'edge',
+  'node': 'node',
+  'ios_saf': 'ios',
+  'android': 'chrome',
+  'and_chr': 'chrome',
+  'and_ff': 'firefox',
+
+  // 'deno': 'deno',
+} as const;
+
+const supportedBrowsers = new Set(Object.keys(browsersToTargets));
+
+function isSupportedBrowser(browser: string): browser is SupportedBrowser {
+  return supportedBrowsers.has(browser);
 }
 
 type LoadTargetOptions = {
@@ -13,26 +29,40 @@ type LoadTargetOptions = {
 };
 
 export async function loadTargets(options: LoadTargetOptions): Promise<string[]> {
-  const baseTargets = browserslist(options.query, {
+  const queries = browserslist(options.query, {
     path: options.basePath,
   });
 
-  const targetVersions = new Map<SupportedPlatform, number>();
+  const targetVersions = new Map<EsbuildTarget, number>();
 
-  for (const target of baseTargets) {
-    const [platform, versionString] = target.split(' ');
-    const minVersion = +versionString.split('-')[0];
+  for (const query of queries) {
+    const [browser, versionString] = query.split(' ');
 
-    if (!isSupported(platform)) {
+    if (!isSupportedBrowser(browser)) {
       continue;
     }
 
-    const targetVersion = targetVersions.get(platform);
+    let target = browsersToTargets[browser];
+    let minVersion = +versionString.split('-')[0];
+
+    if (browser === 'android') {
+      // according to https://developer.android.com/guide/webapps/migrating
+      if (minVersion >= 4.4) {
+        target = 'chrome';
+        // "defaults" minimum chrome version
+        // at 2022.02.26.
+        minVersion = 96;
+      } else {
+        continue;
+      }
+    }
+
+    const targetVersion = targetVersions.get(target);
     if (!targetVersion || targetVersion > minVersion) {
-      targetVersions.set(platform, minVersion);
+      targetVersions.set(target, minVersion);
     }
   }
 
   return Array.from(targetVersions.entries())
-    .map(([platform, version]) => platform + version);
+    .map(targetVersion => targetVersion.join(''));
 }
